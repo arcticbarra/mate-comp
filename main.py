751b6ef77411c41989cd9c86e28857df15e95273
ldvo <14080847+ldvo@users.noreply.github.com>
2017-12-02 12:09:35 -0600

    changes

--- main.py
+++ main.py
@@ -7,17 +7,40 @@
 #   next state (like a goto jump).
 
 #       symbol 0    symbol 1
-prog = [[(1, +1, 1), (1, +1, 0)],           # state 0
-        [(0, -1, 2), (1, +1, 1)],           # state 1
-        [(0, +1, 2), (0, +1, 9)]]           # state 2
-tape = [1, 1, 0, 1, 1, 1, 0, 0, 0]                  # The data tape
-head = 0                                    # head position on tape
-state = 0                                   # instruction pointer
+
+# arreglo de diccionaries para cada estado
+states = [{}, {}, {}, {}]
+
+# states[estado actual][simbolo que lee] = (simbolo que escribe, direccion en que se mueve en la cinta, estado nuevo) 
+states[0]['X'] = ('X', +1, 0)
+states[0]['0'] = ('X', +1, 1)
+states[0]['1'] = ('X', +1, 2)
+states[0]['B'] = ('B', -1, 4)
+states[1]['X'] = ('X', +1, 1)
+states[1]['0'] = ('0', +1, 1)
+states[1]['1'] = ('X', -1, 3)
+states[2]['X'] = ('X', +1, 2)
+states[2]['1'] = ('1', +1, 2)
+states[2]['0'] = ('X', -1, 3)
+states[3]['X'] = ('X', -1, 3)
+states[3]['0'] = ('0', -1, 3)
+states[3]['1'] = ('1', -1, 3)
+states[3]['B'] = ('B', +1, 0)
+
+tape = ['B','1', '1', '0', '1', '1', '0', '0', 'B'] # cinta (se agrega B al principio para simular lista circular)
+head = 1                                    # posicion de la cabeza
+state = 0                                   # estado actual
 print(tape)
-while state != 9:                           # while not halt:
-    symbol = tape[head]                         # read current tape symbol
-    symbol, dir, state = t = prog[state][symbol]  # lookup instruction
-    print(' ' * (head * 3 + 1) + '^  ' + str(t))  # display progress
-    tape[head] = symbol                         # write new symbol on tape
+while state != 4:                           # mientras que no este en el estado de aceptacion
+    symbol = tape[head]                         # lee el simbolo
+    if symbol not in states[state]:
+        print("Rechazada")
+        break
+    symbol, dir, state = t = states[state][symbol]  # obtiene la instruccion
+    print(' ' * (head * 3 + 1) + '^  ' + str(t))  # imprime paso
+    tape[head] = symbol                         # escribe el nuevo simbolo en la cinta
     print(tape)
     head = head + dir
+
+if state == 4:
+    print("Exito")
